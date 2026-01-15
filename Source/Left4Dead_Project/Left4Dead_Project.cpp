857177ebef19148b1725cf6fdd8b7ba133a688d2
dksktlq5-pixel <dksktlq5@gmail.com>
2026-01-15 11:08:41 +0900

    Left4Dead_Project

---
+++ Source/Left4Dead_Project/Left4Dead_Project.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Left4Dead_Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Left4Dead_Project, "Left4Dead_Project" );
